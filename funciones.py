4b78512aed9d1a59c7f588b723ab62d857b841c6
Jared Peter <aljapeter05@gmail.com>
2024-09-03 14:27:37 -0300

    mas explicaciones

--- funciones.py
+++ funciones.py
@@ -1,3 +1,4 @@
+import copy
 # Puntero: una variable que tiene como contenido una direccion a la memoria
 
 a = ["uno", "dos"]
@@ -12,3 +13,31 @@ print(b)
 
 x = a.copy()
 # es una copia de a, apunta a otro espacio en la memoria
+# .copy() copia solamente el primer nivel del array
+print(x)
+
+
+
+
+
+
+
+
+
+
+x = [[1, 2, 3], ['Juan', 'Ana'], True]
+y = x.copy()
+
+# Cada array dentro del array es referencia a otro espacio en la memoria,
+# asi que cuando cambie un elemento de un sub-array del elemento padre,
+# la variable que es una copia tambien va a cambiar, ya que contiene
+# una referencia externa (esta referencia es la que cambio)
+x[0][1] = 333
+
+# deepcopy copia todos los niveles, asi que no importa si cambia un sub elemento
+# en el array que fue copiado, el array que es copia no va a cambiar
+z = copy.deepcopy(x)
+
+
+print(x)
+print(y)
--- listas.py
+++ listas.py
@@ -1,1 +1,12 @@
 a = ['a', 1, True]
+
+# tuplas
+# Son listas pero inmutables
+tup = (1, 2, 3)
+
+inicio, final, paso = 0, 10, 2
+a = range(final)
+print(a)
+
+a = range(inicio, final, paso)
+print(a)
