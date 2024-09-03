65d2053d855057f8b1306f0f0842e5251b8aa6e8
Jared Peter <aljapeter05@gmail.com>
2024-09-03 14:07:04 -0300

    new change

--- funciones.py
+++ funciones.py
@@ -1,4 +1,14 @@
 # Puntero: una variable que tiene como contenido una direccion a la memoria
 
-a = 1
+a = ["uno", "dos"]
 print(id(a))
+
+b = a
+print(id(a))
+
+a[0] = "tres"
+print(b)
+# b tambien cambia, ya que tienen la misma direccion de memoria con b
+
+x = a.copy()
+# es una copia de a, apunta a otro espacio en la memoria
